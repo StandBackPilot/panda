84a4a5f7381bc0d17adf495519479070c4b5c78c
Jason Shuler <jshuler@gmail.com>
2022-10-06 13:38:34 -0400

    GM CC lateral-only support

--- board/safety/safety_gm.h
+++ board/safety/safety_gm.h
@@ -28,6 +28,8 @@ const CanMsg GM_ASCM_TX_MSGS[] = {{384, 0, 4}, {1033, 0, 7}, {1034, 0, 7}, {715,
 const CanMsg GM_CAM_TX_MSGS[] = {{384, 0, 4},  // pt bus
                                  {481, 2, 7}};  // camera bus
 
+const CanMsg GM_CAM_CC_TX_MSGS[] = {{384, 0, 4}, {481, 0, 7}};  // pt bus
+
 // TODO: do checksum and counter checks. Add correct timestep, 0.1s for now.
 AddrCheckStruct gm_addr_checks[] = {
   {.msg = {{388, 0, 8, .expected_timestep = 100000U}, { 0 }, { 0 }}},
@@ -40,6 +42,7 @@ AddrCheckStruct gm_addr_checks[] = {
 addr_checks gm_rx_checks = {gm_addr_checks, GM_RX_CHECK_LEN};
 
 const uint16_t GM_PARAM_HW_CAM = 1;
+const uint16_t GM_PARAM_HW_CAM_CC = 4;
 
 enum {
   GM_BTN_UNPRESS = 1,
@@ -48,7 +51,7 @@ enum {
   GM_BTN_CANCEL = 6,
 };
 
-enum {GM_ASCM, GM_CAM} gm_hw = GM_ASCM;
+enum {GM_ASCM, GM_CAM, GM_CAM_CC} gm_hw = GM_ASCM;
 
 static int gm_rx_hook(CANPacket_t *to_push) {
 
@@ -105,6 +108,13 @@ static int gm_rx_hook(CANPacket_t *to_push) {
       }
     }
 
+    // Standard CC state is in ECMEngineStatus
+    // enter controls on rising edge of CC, exit controls when CC off
+    if ((addr == 201) && (gm_hw == GM_CAM_CC)) {
+      bool cruise_engaged = (GET_BYTE(to_push, 3) >> 6) == 1U;
+      pcm_cruise_check(cruise_engaged);
+    }
+
     if (addr == 189) {
       regen_braking = (GET_BYTE(to_push, 0) >> 4) != 0U;
     }
@@ -133,6 +143,8 @@ static int gm_tx_hook(CANPacket_t *to_send, bool longitudinal_allowed) {
 
   if (gm_hw == GM_CAM) {
     tx = msg_allowed(to_send, GM_CAM_TX_MSGS, sizeof(GM_CAM_TX_MSGS)/sizeof(GM_CAM_TX_MSGS[0]));
+  } else if (gm_hw == GM_CAM_CC) {
+    tx = msg_allowed(to_send, GM_CAM_CC_TX_MSGS, sizeof(GM_CAM_CC_TX_MSGS)/sizeof(GM_CAM_CC_TX_MSGS[0]));
   } else {
     tx = msg_allowed(to_send, GM_ASCM_TX_MSGS, sizeof(GM_ASCM_TX_MSGS)/sizeof(GM_ASCM_TX_MSGS[0]));
   }
@@ -232,11 +244,18 @@ static int gm_tx_hook(CANPacket_t *to_send, bool longitudinal_allowed) {
   }
 
   // BUTTONS: used for resume spamming and cruise cancellation with stock longitudinal
-  if ((addr == 481) && (gm_hw == GM_CAM)) {
+  if ((addr == 481) && (gm_hw == GM_CAM || gm_hw == GM_CAM_CC)) {
     int button = (GET_BYTE(to_send, 5) >> 4) & 0x7U;
 
     bool allowed_cancel = (button == 6) && cruise_engaged_prev;
     if (!allowed_cancel) {
+      tx = 0;
+    }
+
+    // TODO: Establish conditions for button spams in CC mode
+    // Note: in CC mode, allow all button presses for now
+    bool allowed_button = (gm_hw == GM_CAM_CC);
+    if (!allowed_button) {
       tx = 0;
     }
   }
@@ -249,7 +268,7 @@ static int gm_fwd_hook(int bus_num, CANPacket_t *to_fwd) {
 
   int bus_fwd = -1;
 
-  if (gm_hw == GM_CAM) {
+  if (gm_hw == GM_CAM || gm_hw == GM_CAM_CC) {
     if (bus_num == 0) {
       bus_fwd = 2;
     }
@@ -269,6 +288,9 @@ static int gm_fwd_hook(int bus_num, CANPacket_t *to_fwd) {
 
 static const addr_checks* gm_init(uint16_t param) {
   gm_hw = GET_FLAG(param, GM_PARAM_HW_CAM) ? GM_CAM : GM_ASCM;
+  if (gm_hw == GM_CAM && GET_FLAG(param, GM_PARAM_HW_CAM_CC)) {
+    gm_hw = GM_CAM_CC;
+  }
   return &gm_rx_checks;
 }
 
