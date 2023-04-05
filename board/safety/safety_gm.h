0c37ffda1826bfe798ff3f0603e21e619ad935a2
Rick Murphy <k1mu@arrl.net>
2023-04-05 19:44:36 -0400

    Updates for redneck panda changes

--- board/safety/safety_gm.h
+++ board/safety/safety_gm.h
@@ -1,40 +1,47 @@
-// board enforces
-//   in-state
-//      accel set/resume
-//   out-state
-//      cancel button
-//      regen paddle
-//      accel rising edge
-//      brake rising edge
-//      brake > 0mph
+const SteeringLimits GM_STEERING_LIMITS = {
+  .max_steer = 300,
+  .max_rate_up = 10,
+  .max_rate_down = 15,
+  .driver_torque_allowance = 65,
+  .driver_torque_factor = 4,
+  .max_rt_delta = 128,
+  .max_rt_interval = 250000,
+  .type = TorqueDriverLimited,
+};
 
-const int GM_MAX_STEER = 300;
-const int GM_MAX_RT_DELTA = 128;          // max delta torque allowed for real time checks
-const uint32_t GM_RT_INTERVAL = 250000;    // 250ms between real time checks
-const int GM_MAX_RATE_UP = 7;
-const int GM_MAX_RATE_DOWN = 17;
-const int GM_DRIVER_TORQUE_ALLOWANCE = 50;
-const int GM_DRIVER_TORQUE_FACTOR = 4;
-const int GM_MAX_GAS = 3072;
-const int GM_MAX_REGEN = 1404;
-const int GM_MAX_BRAKE = 350;
+const LongitudinalLimits GM_ASCM_LONG_LIMITS = {
+  .max_gas = 3072,
+  .min_gas = 1404,
+  .inactive_gas = 1404,
+  .max_brake = 400,
+};
 
+const LongitudinalLimits GM_CAM_LONG_LIMITS = {
+  .max_gas = 3400,
+  .min_gas = 1514,
+  .inactive_gas = 1554,
+  .max_brake = 400,
+};
 
-int gm_good_cam_cnt = 0;
-bool gm_allow_fwd = true;
-bool gm_block_fwd = false;
-int gm_camera_bus = 2;
-bool gm_has_relay = true;
-uint32_t gm_start_ts = 0;
-uint32_t gm_last_lkas_ts = 0;
+const LongitudinalLimits *gm_long_limits;
 
-const int GM_GAS_INTERCEPTOR_THRESHOLD = 458;  // (610 + 306.25) / 2ratio between offset and gain from dbc file
-#define GM_GET_INTERCEPTOR(msg) (((GET_BYTE((msg), 0) << 8) + GET_BYTE((msg), 1) + (GET_BYTE((msg), 2) << 8) + GET_BYTE((msg), 3)) / 2) // avg between 2 tracks
+const int GM_STANDSTILL_THRSLD = 10;  // 0.311kph
 
-const CanMsg GM_TX_MSGS[] = {{384, 0, 4}, {1033, 0, 7}, {1034, 0, 7}, {715, 0, 8}, {880, 0, 6}, {512, 0, 6}, {789, 0, 5}, {800, 0, 6},  // pt bus
-                             {161, 1, 7}, {774, 1, 8}, {776, 1, 7}, {784, 1, 2},   // obs bus
-                             {789, 2, 5},  // ch bus
-                             {0x104c006c, 3, 3}, {0x10400060, 3, 5}};  // gmlan
+// panda interceptor threshold needs to be equivalent to openpilot threshold to avoid controls mismatches
+// If thresholds are mismatched then it is possible for panda to see the gas fall and rise while openpilot is in the pre-enabled state
+const int GM_GAS_INTERCEPTOR_THRESHOLD = 506; // (610 + 306.25) / 2 ratio between offset and gain from dbc file
+#define GM_GET_INTERCEPTOR(msg) (((GET_BYTE((msg), 0) << 8) + GET_BYTE((msg), 1) + (GET_BYTE((msg), 2) << 8) + GET_BYTE((msg), 3)) / 2U) // avg between 2 tracks
+
+const CanMsg GM_ASCM_TX_MSGS[] = {{384, 0, 4}, {1033, 0, 7}, {1034, 0, 7}, {715, 0, 8}, {880, 0, 6}, {512, 0, 6},  // pt bus
+                                  {161, 1, 7}, {774, 1, 8}, {776, 1, 7}, {784, 1, 2},   // obs bus
+                                  {789, 2, 5},  // ch bus
+                                  {0x104c006c, 3, 3}, {0x10400060, 3, 5}};  // gmlan
+
+const CanMsg GM_CAM_TX_MSGS[] = {{384, 0, 4}, {512, 0, 6}, {481, 0, 7},  // pt bus
+                                 {481, 2, 7}, {388, 2, 8}};  // camera bus
+
+const CanMsg GM_CAM_LONG_TX_MSGS[] = {{384, 0, 4}, {789, 0, 5}, {715, 0, 8}, {880, 0, 6}, {512, 0, 6}, {481, 0, 7}, // pt bus
+                                      {481, 2, 7}, {388, 2, 8}};  // camera bus
 
 // TODO: do checksum and counter checks. Add correct timestep, 0.1s for now.
 AddrCheckStruct gm_addr_checks[] = {
@@ -42,11 +49,17 @@ AddrCheckStruct gm_addr_checks[] = {
   {.msg = {{842, 0, 5, .expected_timestep = 100000U}, { 0 }, { 0 }}},
   {.msg = {{481, 0, 7, .expected_timestep = 100000U}, { 0 }, { 0 }}},
   {.msg = {{241, 0, 6, .expected_timestep = 100000U}, { 0 }, { 0 }}},
+  {.msg = {{190, 0, 6, .expected_timestep = 100000U},    // Volt, Silverado, Acadia Denali
+           {190, 0, 7, .expected_timestep = 100000U},    // Bolt EUV
+           {190, 0, 8, .expected_timestep = 100000U}}},  // Escalade
   {.msg = {{452, 0, 8, .expected_timestep = 100000U}, { 0 }, { 0 }}},
+  {.msg = {{201, 0, 8, .expected_timestep = 100000U}, { 0 }, { 0 }}},
 };
 #define GM_RX_CHECK_LEN (sizeof(gm_addr_checks) / sizeof(gm_addr_checks[0]))
 addr_checks gm_rx_checks = {gm_addr_checks, GM_RX_CHECK_LEN};
 
+const uint16_t GM_PARAM_HW_CAM = 1;
+const uint16_t GM_PARAM_HW_CAM_LONG = 2;
 
 enum {
   GM_BTN_UNPRESS = 1,
@@ -55,9 +68,13 @@ enum {
   GM_BTN_CANCEL = 6,
 };
 
+enum {GM_ASCM, GM_CAM} gm_hw = GM_ASCM;
+bool gm_cam_long = false;
+bool gm_pcm_cruise = false;
+
 static int gm_rx_hook(CANPacket_t *to_push) {
 
-  bool valid = addr_safety_check(to_push, &gm_rx_checks, NULL, NULL, NULL);
+  bool valid = addr_safety_check(to_push, &gm_rx_checks, NULL, NULL, NULL, NULL);
 
   if (valid && (GET_BUS(to_push) == 0U)) {
     int addr = GET_ADDR(to_push);
@@ -69,68 +86,74 @@ static int gm_rx_hook(CANPacket_t *to_push) {
       update_sample(&torque_driver, torque_driver_new);
     }
 
-    // sample speed, really only care if car is moving or not
-    // rear left wheel speed
+    // sample rear wheel speeds
     if (addr == 842) {
-      vehicle_moving = GET_BYTE(to_push, 0) | GET_BYTE(to_push, 1);
+      int left_rear_speed = (GET_BYTE(to_push, 0) << 8) | GET_BYTE(to_push, 1);
+      int right_rear_speed = (GET_BYTE(to_push, 2) << 8) | GET_BYTE(to_push, 3);
+      vehicle_moving = (left_rear_speed > GM_STANDSTILL_THRSLD) || (right_rear_speed > GM_STANDSTILL_THRSLD);
     }
 
-    //TODO: Should we be checking the CC status?
-
-    // ACC steering wheel buttons
-    if (addr == 481) {
+    // ACC steering wheel buttons (GM_CAM is tied to the PCM)
+    if ((addr == 481) && (gm_hw == GM_CAM)) {
       int button = (GET_BYTE(to_push, 5) & 0x70U) >> 4;
+
+      // enter controls on falling edge of set or rising edge of resume (avoids fault)
+      bool set = (button != GM_BTN_SET) && (cruise_button_prev == GM_BTN_SET);
+      bool res = (button == GM_BTN_RESUME) && (cruise_button_prev != GM_BTN_RESUME);
+      if (set || res) {
+        controls_allowed = 1;
+      }
 
       // exit controls on cancel press
       if (button == GM_BTN_CANCEL) {
         controls_allowed = 0;
       }
 
-      // enter controls on falling edge of set or resume
-      bool set = (button == GM_BTN_UNPRESS) && (cruise_button_prev == GM_BTN_SET);
-      bool res = (button == GM_BTN_UNPRESS) && (cruise_button_prev == GM_BTN_RESUME);
-      if (set || res) {
-        controls_allowed = 1;
-      }
-
       cruise_button_prev = button;
     }
 
-    // speed > 0
-    if (addr == 241) {
-      // Brake pedal's potentiometer returns near-zero reading
-      // even when pedal is not pressed
-      brake_pressed = GET_BYTE(to_push, 1) >= 11U;
+    // Reference for brake pressed signals:
+    // https://github.com/commaai/openpilot/blob/master/selfdrive/car/gm/carstate.py
+    if ((addr == 190) && (gm_hw == GM_ASCM)) {
+      brake_pressed = GET_BYTE(to_push, 1) >= 8U;
+    }
+
+    if ((addr == 201) && (gm_hw == GM_CAM)) {
+      brake_pressed = GET_BIT(to_push, 40U) != 0U;
     }
 
     if (addr == 452) {
-      gas_pressed = GET_BYTE(to_push, 5) != 0U;
+      if (!gas_interceptor_detected) {
+        gas_pressed = GET_BYTE(to_push, 5) != 0U;
+      }
+
+      // enter controls on rising edge of ACC, exit controls when ACC off
+      if (gm_pcm_cruise) {
+        bool cruise_engaged = (GET_BYTE(to_push, 1) >> 5) != 0U;
+        pcm_cruise_check(cruise_engaged);
+      }
     }
 
-    // exit controls on regen paddle
-    //TODO: Evaluate impact of this change. Previous method could have caused controls mismatch...
     if (addr == 189) {
-      brake_pressed = GET_BYTE(to_push, 0) & 0x20U;
-      // if (regen) {
-      //   controls_allowed = 0;
-      // }
+      regen_braking = (GET_BYTE(to_push, 0) >> 4) != 0U;
     }
 
     // Pedal Interceptor
     if (addr == 513) {
       gas_interceptor_detected = 1;
+      gm_pcm_cruise = false;
       int gas_interceptor = GM_GET_INTERCEPTOR(to_push);
       gas_pressed = gas_interceptor > GM_GAS_INTERCEPTOR_THRESHOLD;
       gas_interceptor_prev = gas_interceptor;
     }
 
-    // Check if ASCM or LKA camera are online
-    // on powertrain bus.
-    // 384 = ASCMLKASteeringCmd
-    // 715 = ASCMGasRegenCmd
-    //generic_rx_checks(((addr == 384) || (addr == 715)));
-    generic_rx_checks(addr == 384);
-    //TODO: relay malfunction firing when 715 is stock
+    bool stock_ecu_detected = (addr == 384);  // ASCMLKASteeringCmd
+
+    // Only check ASCMGasRegenCmd if ASCM, GM_CAM uses stock longitudinal
+    if ((gm_hw == GM_ASCM) && (addr == 715)) {
+      stock_ecu_detected = true;
+    }
+    generic_rx_checks(stock_ecu_detected);
   }
   return valid;
 }
@@ -141,143 +164,72 @@ static int gm_rx_hook(CANPacket_t *to_push) {
 // else
 //     block all commands that produce actuation
 
-static int gm_tx_hook(CANPacket_t *to_send, bool longitudinal_allowed) {
+static int gm_tx_hook(CANPacket_t *to_send) {
 
   int tx = 1;
   int addr = GET_ADDR(to_send);
 
-  if (!msg_allowed(to_send, GM_TX_MSGS, sizeof(GM_TX_MSGS)/sizeof(GM_TX_MSGS[0]))) {
-    tx = 0;
-  }
-
-  // disallow actuator commands if gas or brake (with vehicle moving) are pressed
-  // and the the latching controls_allowed flag is True
-  int pedal_pressed = brake_pressed_prev && vehicle_moving;
-  bool alt_exp_allow_gas = alternative_experience & ALT_EXP_DISABLE_DISENGAGE_ON_GAS;
-  if (!alt_exp_allow_gas) {
-    pedal_pressed = pedal_pressed || gas_pressed_prev;
-  }
-  bool current_controls_allowed = controls_allowed && !pedal_pressed;
-
-  // GAS: safety check (interceptor)
-  if (addr == 512) {
-    if (!current_controls_allowed) {
-      if (GET_BYTE(to_send, 0) || GET_BYTE(to_send, 1)) {
-        tx = 0;
-      }
+  if (gm_hw == GM_CAM) {
+    if (gm_cam_long) {
+      tx = msg_allowed(to_send, GM_CAM_LONG_TX_MSGS, sizeof(GM_CAM_LONG_TX_MSGS)/sizeof(GM_CAM_LONG_TX_MSGS[0]));
+    } else {
+      tx = msg_allowed(to_send, GM_CAM_TX_MSGS, sizeof(GM_CAM_TX_MSGS)/sizeof(GM_CAM_TX_MSGS[0]));
     }
+  } else {
+    tx = msg_allowed(to_send, GM_ASCM_TX_MSGS, sizeof(GM_ASCM_TX_MSGS)/sizeof(GM_ASCM_TX_MSGS[0]));
   }
 
   // BRAKE: safety check
   if (addr == 789) {
     int brake = ((GET_BYTE(to_send, 0) & 0xFU) << 8) + GET_BYTE(to_send, 1);
     brake = (0x1000 - brake) & 0xFFF;
-    if (!current_controls_allowed || !longitudinal_allowed) {
-      if (brake != 0) {
-        tx = 0;
-      }
-    }
-    if (brake > GM_MAX_BRAKE) {
+    if (longitudinal_brake_checks(brake, *gm_long_limits)) {
       tx = 0;
     }
   }
 
-  // // LKA STEER: safety check
-  // if (addr == 384) {
-  //   //int rolling_counter = GET_BYTE(to_send, 0) >> 4;
-  //   int desired_torque = ((GET_BYTE(to_send, 0) & 0x7U) << 8) + GET_BYTE(to_send, 1);
-  //   uint32_t ts = microsecond_timer_get();
-  //   bool violation = 0;
-  //   desired_torque = to_signed(desired_torque, 11);
-
-  //   if (current_controls_allowed) {
-
-  //     // *** global torque limit check ***
-  //     violation |= max_limit_check(desired_torque, GM_MAX_STEER, -GM_MAX_STEER);
-
-  //     // *** torque rate limit check ***
-  //     violation |= driver_limit_check(desired_torque, desired_torque_last, &torque_driver,
-  //       GM_MAX_STEER, GM_MAX_RATE_UP, GM_MAX_RATE_DOWN,
-  //       GM_DRIVER_TORQUE_ALLOWANCE, GM_DRIVER_TORQUE_FACTOR);
-
-  //     // used next time
-  //     desired_torque_last = desired_torque;
-
-  //     // TODO: JJS: Reenable after finding better numbers
-  //     // // *** torque real time rate limit check ***
-  //     // violation |= rt_rate_limit_check(desired_torque, rt_torque_last, GM_MAX_RT_DELTA);
-
-  //     // // every RT_INTERVAL set the new limits
-  //     // uint32_t ts_elapsed = get_ts_elapsed(ts, ts_last);
-  //     // if (ts_elapsed > GM_RT_INTERVAL) {
-  //     //   rt_torque_last = desired_torque;
-  //     //   ts_last = ts;
-  //     // }
-  //   }
-
-  //   // no torque if controls is not allowed
-  //   if (!current_controls_allowed && (desired_torque != 0)) {
-  //     violation = 1;
-  //   }
-
-  //   // reset to 0 if either controls is not allowed or there's a violation
-  //   if (violation || !current_controls_allowed) {
-  //     desired_torque_last = 0;
-  //     rt_torque_last = 0;
-  //     ts_last = ts;
-  //   }
-
-  //   if (violation) {
-  //     tx = 0;
-  //   }
-
-  //   //Last chance to catch too-soon frame
-  //   if (tx == 1) {
-  //     uint32_t ts2 = microsecond_timer_get();
-  //     uint32_t ts_elapsed = get_ts_elapsed(ts2, gm_last_lkas_ts);
-  //     if (ts_elapsed <= 13000) { // Should be every 20ms, but it seems to tolerate down lower
-  //       // Hard 20ms cutoff was dropping WAY too many frames
-  //       tx = 0;
-  //     }
-  //     else {
-  //       gm_last_lkas_ts = ts2;
-  //     }
-  //   }
-
-  // }
-
-  //Last chance to catch too-soon frame
+  // LKA STEER: safety check
   if (addr == 384) {
-    uint32_t ts2 = microsecond_timer_get();
-    uint32_t ts_elapsed = get_ts_elapsed(ts2, gm_last_lkas_ts);
-    if (ts_elapsed <= 13000) { // Should be every 20ms, but it seems to tolerate down lower
-      // Hard 20ms cutoff was dropping WAY too many frames
+    int desired_torque = ((GET_BYTE(to_send, 0) & 0x7U) << 8) + GET_BYTE(to_send, 1);
+    desired_torque = to_signed(desired_torque, 11);
+
+    if (steer_torque_cmd_checks(desired_torque, -1, GM_STEERING_LIMITS)) {
       tx = 0;
     }
-    else {
-      gm_last_lkas_ts = ts2;
+  }
+
+  // GAS: safety check (interceptor)
+  if (addr == 512) {
+    if (longitudinal_interceptor_checks(to_send)) {
+      tx = 0;
     }
   }
 
   // GAS/REGEN: safety check
   if (addr == 715) {
+    bool apply = GET_BIT(to_send, 0U) != 0U;
     int gas_regen = ((GET_BYTE(to_send, 2) & 0x7FU) << 5) + ((GET_BYTE(to_send, 3) & 0xF8U) >> 3);
-    // Disabled message is !engaged with gas
-    // value that corresponds to max regen.
-    if (!current_controls_allowed || !longitudinal_allowed) {
-      // Stock ECU sends max regen when not enabled
-      if (gas_regen != GM_MAX_REGEN) {
-        tx = 0;
-      }
+
+    bool violation = false;
+    // Allow apply bit in pre-enabled and overriding states
+    violation |= !controls_allowed && apply;
+    violation |= longitudinal_gas_checks(gas_regen, *gm_long_limits);
+
+    if (violation) {
+      tx = 0;
     }
-    // Need to allow apply bit in pre-enabled and overriding states
-    if (!controls_allowed) {
-      bool apply = GET_BIT(to_send, 0U) != 0U;
-      if (apply) {
-        tx = 0;
-      }
-    }
-    if (gas_regen > GM_MAX_GAS) {
+  }
+
+  // BUTTONS: used for resume spamming and cruise cancellation with stock longitudinal
+  if ((addr == 481) && (gm_pcm_cruise || gm_hw == GM_CAM)) {
+    int button = (GET_BYTE(to_send, 5) >> 4) & 0x7U;
+
+    bool allowed_btn = (button == GM_BTN_CANCEL) && cruise_engaged_prev;
+    // For standard CC, allow spamming of SET / RESUME
+    allowed_btn |= cruise_engaged_prev && (gm_hw == GM_CAM) && (button == GM_BTN_SET || button == GM_BTN_RESUME || button == GM_BTN_UNPRESS);
+    // TODO: With a Pedal, CC needs to be canceled
+
+    if (!allowed_btn) {
       tx = 0;
     }
   }
@@ -286,70 +238,25 @@ static int gm_tx_hook(CANPacket_t *to_send, bool longitudinal_allowed) {
   return tx;
 }
 
-
-static void gm_validate_camera(int addr, CANPacket_t *to_fwd) {
-  //TODO: Add more known good camera message ids
-  if (addr == 384) {
-    int len = GET_LEN(to_fwd);
-    if (len == 4) {
-      gm_allow_fwd = true;
-      gm_good_cam_cnt++;
-    }
-    else {
-      gm_good_cam_cnt = 0;
-      gm_block_fwd = true;
-    }
-  }
-  else if ((addr == 1120) // F_LRR_Obj_Header from object bus
-        || (addr == 784) // ASCMHeadlight from object bus
-        || (addr == 309) // LHT_CameraObjConfirmation_FO from object bus
-        || (addr == 192) // Unknown id only on chassis bus
-  ) {
-    gm_good_cam_cnt = 0;
-    gm_block_fwd = true;
-  }
-
-  // If we are forwarding, but we have seen no LKAS frames on cam bus for 3 seconds, stop forwarding!
-  if (gm_allow_fwd && (gm_good_cam_cnt <= 0)) {
-    uint32_t ts = microsecond_timer_get();
-    uint32_t ts_elapsed = get_ts_elapsed(ts, gm_start_ts);
-    if (ts_elapsed > 3000000) {
-      gm_allow_fwd = false;
-    }
-  }
-
-}
-
 static int gm_fwd_hook(int bus_num, CANPacket_t *to_fwd) {
+
   int bus_fwd = -1;
-  // TODO JJS: Forwarder FAILS MISERABLY when there is no camera
 
-  if (bus_num == 0) {
-    if (gm_allow_fwd && !gm_block_fwd) {
-      bus_fwd = gm_camera_bus;
-    }
-  }
-  else if (bus_num == gm_camera_bus) {
+  if (gm_hw == GM_CAM) {
     int addr = GET_ADDR(to_fwd);
-
-    // Do extra testing on frames from the cam bus untill we have seen 2 good LKAS frames
-    // If any bad frames are encountered before then, forwarding is blocked
-    // For OBD-based connections, forwarding defaults to off and is enabled if we see good LKAS
-    // For harness-based connections, forwarding defaults to on and is disabled if we see bad frames
-    // TODO: Can this all be determined by OP and sent as a param?
-    // OP FP may be able to detect all of this...
-    // This testing is limited in duration to reduce load
-    if (!gm_block_fwd && gm_good_cam_cnt <= 2) {
-      gm_validate_camera(addr, to_fwd);
+    if (bus_num == 0) {
+      // block PSCMStatus; forwarded through openpilot to hide an alert from the camera
+      bool is_pscm_msg = (addr == 388);
+      if (!is_pscm_msg) {
+        bus_fwd = 2;
+      }
     }
-    
-    if (gm_allow_fwd && !gm_block_fwd) {
-      // block stock lkas messages and stock acc messages (if OP is doing ACC)
-      //TODO: Blocking stock camera ACC will need to be an option in custom fork to allow use of OP's VOACC.
-      int is_lkas_msg = (addr == 384);
-      int is_acc_msg = false;
-      //int is_acc_msg = (addr == 0x343);
-      int block_msg = is_lkas_msg || is_acc_msg;
+
+    if (bus_num == 2) {
+      // block lkas message and acc messages if gm_cam_long, forward all others
+      bool is_lkas_msg = (addr == 384);
+      bool is_acc_msg = (addr == 789) || (addr == 715) || (addr == 880);
+      int block_msg = is_lkas_msg || (is_acc_msg && gm_cam_long);
       if (!block_msg) {
         bus_fwd = 0;
       }
@@ -360,26 +267,19 @@ static int gm_fwd_hook(int bus_num, CANPacket_t *to_fwd) {
 }
 
 static const addr_checks* gm_init(uint16_t param) {
-  UNUSED(param);
-  gm_good_cam_cnt = 0;
-  gm_allow_fwd = true;
-  gm_block_fwd = false;
-  gm_camera_bus = 2;
-  gm_has_relay = true;
-  gm_start_ts = microsecond_timer_get();
+  gm_hw = GET_FLAG(param, GM_PARAM_HW_CAM) ? GM_CAM : GM_ASCM;
 
-  if (car_harness_status == HARNESS_STATUS_NC) {
-    //TODO: It seems as though the OBD2 harness may present as having a harness w relay.
-    // So this may not work to detect a relay as previously thought
-    // Seems to work with grey panda tho
-    //puts("gm_init: No harness attached, assuming OBD or Giraffe\n");
-    //OBD harness and older pandas use bus 1 and no relay
-    //Most likely if we are using the OBD Harness, we have an ASCM and don't want to forward.
-    gm_has_relay = false;
-    gm_camera_bus = 1;
-    gm_allow_fwd = false;
+  if (gm_hw == GM_ASCM) {
+    gm_long_limits = &GM_ASCM_LONG_LIMITS;
+  } else if (gm_hw == GM_CAM) {
+    gm_long_limits = &GM_CAM_LONG_LIMITS;
+  } else {
   }
 
+#ifdef ALLOW_DEBUG
+  gm_cam_long = GET_FLAG(param, GM_PARAM_HW_CAM_LONG);
+#endif
+  gm_pcm_cruise = (gm_hw == GM_CAM) && !gm_cam_long;
   return &gm_rx_checks;
 }
 
